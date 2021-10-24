c487e2be51d99189b3a16312b9024ea09bcb8e9f
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2021-10-24 15:50:26 +0100

    fix: Minor code tweak

--- common/createDirStructure.sh
+++ common/createDirStructure.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-desiredRoot="/home/$(whoami)"
-
 createDirIfDoesNotExist() {
-  [ ! -d "$desiredRoot/$1" ] && mkdir "$desiredRoot/$1"
+  [ ! -d "$HOME/$1" ] && mkdir "$HOME/$1"
 }
 
 directories=("customizations" ".themes" "Desktop/olaolu_dev" "Desktop/olaolu_dev/dev" "Desktop/olaolu_dev/learnings" "Desktop/olaolu_dev/scripts" "AppImages" "Downloads/rpms" "Downloads/others")
@@ -12,7 +10,7 @@ echo "Creating directories"
 
 for directory in "${directories[@]}"; do
   createDirIfDoesNotExist "$directory"
-  echo "$desiredRoot/$directory created"
+  echo "$HOME/$directory created"
 done
 
 echo "Directories created successfully"
