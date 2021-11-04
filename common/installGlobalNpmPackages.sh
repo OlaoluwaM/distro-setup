4fa7cf51ccb8e009a37287264d707851cbe5bf23
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2021-11-04 07:28:47 +0100

    chore: Added 'nativefier' to list of gloabl npm packages that need installation

--- common/installGlobalNpmPackages.sh
+++ common/installGlobalNpmPackages.sh
@@ -5,7 +5,7 @@ source "$commonScriptsDir/isInstalled.sh"
 
 if [ "$(isInstalled "command -v node")" ] && [ "$(isInstalled "command -v npm")" ]; then
   echo "Installing some global npm packages"
-  packages=("spaceship" "typescript" "create-react-app" "serve" "fkill-cli" "@bitwarden/cli" "term-of-the-day" "@types/node" "netlify-cli" "ntl")
+  packages=("spaceship" "typescript" "create-react-app" "serve" "fkill-cli" "@bitwarden/cli" "term-of-the-day" "@types/node" "netlify-cli" "nativefier")
 
   for package in "${packages[@]}"; do
     echo "Installing $package"
