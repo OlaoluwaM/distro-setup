3a1363c1c5766a460b22377ff565e9cb9d863584
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2021-11-07 07:48:41 +0100

    fix: Issue with snap setup

--- common/cloneGitRepos.sh
+++ common/cloneGitRepos.sh
@@ -24,6 +24,7 @@ if command -v git &>/dev/null || command -v gh &>/dev/null; then
 
     if [ -d "$devPath/$repo" ]; then
       echo "Oh, it looks like $repo has already been cloned, skipping..."
+      printf "\n"
       continue
     fi
 
@@ -52,39 +53,43 @@ if command -v git &>/dev/null || command -v gh &>/dev/null; then
     else
       echo "dotfiles already cloned"
     fi
+    printf "\n"
 
     if [ ! -d "$gnomeThemePath/WhiteSur-gtk-theme" ]; then
       gh repo clone "vinceliuice/WhiteSur-gtk-theme" "$gnomeThemePath/WhiteSur-gtk-theme"
     else
       echo "Looks like WhiteSur-gtk-theme has already been cloned. Skipping..."
     fi
+    printf "\n"
 
     if [ ! -d "$dotFilesPath/scripts" ]; then
       gh repo clone "OlaoluwaM/dev-scripts" "$dotFilesPath/scripts"
     else
       echo "scripts already cloned"
     fi
+    printf "\n"
+    
   else
     if [ ! -d "$dotFilesPath/dotfiles" ]; then
       git clone "git@github.com:OlaoluwaM/dotfiles.git" "$dotFilesPath/dotfiles"
     else
       echo "dotfiles already cloned"
     fi
+    printf "\n"
 
     if [ ! -d "$gnomeThemePath/WhiteSur-gtk-theme" ]; then
       git clone "git@github.com:vinceliuice/WhiteSur-gtk-theme.git" "$gnomeThemePath/WhiteSur-gtk-theme"
     else
       echo "Looks like WhiteSur-gtk-theme has already been cloned. Skipping..."
     fi
+    printf "\n"
 
     if [ ! -d "$dotFilesPath/scripts" ]; then
       git clone "git@github.com:OlaoluwaM/dev-scripts.git" "$dotFilesPath/scripts"
     else
       echo "scripts already cloned"
     fi
-
   fi
-
   if [[ $? -eq 0 ]]; then
     echo "Cloning Complete"
   else
@@ -92,6 +97,7 @@ if command -v git &>/dev/null || command -v gh &>/dev/null; then
     exit 1
   fi
   printf "\n"
+
 else
   echo "Seems like neither git not the Github CLI (gh) are installed."
   exit 1
--- common/setupSnapcraft.sh
+++ common/setupSnapcraft.sh
@@ -5,43 +5,45 @@
 sudo dnf update -y
 printf "\n"
 
-if command -v snap &>/dev/null; then
+if ! command -v snap &>/dev/null; then
+  echo "Looks like snapd hasn't been installed. Installing..."
+  sudo dnf install -y snapd
+  echo "Done"
+  printf "\n"
+fi
+
+if ! systemctl status snapd &>/dev/null; then
+  echo "Setting up snap...."
+  sudo systemctl restart snapd
+  sudo ln -s /var/lib/snapd/snap /snap
+  sudo snap install core
 
   if ! systemctl status snapd &>/dev/null; then
-    echo "Setting up snap...."
-    sudo systemctl restart snapd
-    sudo ln -s /var/lib/snapd/snap /snap
-    sudo snap install core
-
-    if ! systemctl status snapd &>/dev/null; then
-      echo "Hmmm, seems like the snapd is not up, fixing...."
-      sudo systemctl start snap
-      echo "Done"
-    fi
-
-    echo "Setup complete"
+    echo "Hmmm, seems like the snapd is not up, fixing...."
+    sudo systemctl start snap
+    echo "Done"
   fi
 
-  echo "Installing a few snaps...."
-
-  snapsToInstall=("audible-for-linux" "scrcpy")
-
-  echo "Installing some snaps"
-
-  for snapToInstall in "${snapsToInstall[@]}"; do
-    if (snap list | grep "$snapToInstall") &>/dev/null; then
-      echo "Seems like $snapToInstall has already been installed. Skipping...."
-    else
-      echo "Installing $snapToInstall..."
-      sudo snap install "$snapToInstall" -y
-      [[ $? -eq 0 ]] && echo "Successfully installed $snapToInstall"
-    fi
-    printf "\n"
-  done
-
-  echo "Snap setup complete"
-else
-  echo "Looks like snap hasn't been installed on your system :/"
-  exit 1
+  echo "Setup complete. You will need to log out and back then re-run the script"
+  exit 0
 fi
+
+echo "Installing a few snaps...."
+
+snapsToInstall=("audible-for-linux" "scrcpy")
+
+echo "Installing some snaps"
+
+for snapToInstall in "${snapsToInstall[@]}"; do
+  if (snap list | grep "$snapToInstall") &>/dev/null; then
+    echo "Seems like $snapToInstall has already been installed. Skipping...."
+  else
+    echo "Installing $snapToInstall..."
+    sudo snap install "$snapToInstall"
+    [[ $? -eq 0 ]] && echo "Successfully installed $snapToInstall"
+  fi
+  printf "\n"
+done
+
+echo "Snap setup complete"
 printf "\n"
