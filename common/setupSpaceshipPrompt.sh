ba60693afd6f7c236515f63a26d767da73491239
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2021-11-07 15:13:28 +0100

    chore: Fixed issue with spaceship prompt

--- common/setupSpaceshipPrompt.sh
+++ common/setupSpaceshipPrompt.sh
@@ -3,5 +3,5 @@
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" ]; then
   git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
 
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
+  ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
 fi
