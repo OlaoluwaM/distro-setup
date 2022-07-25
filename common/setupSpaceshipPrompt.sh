14380d7f78669f3716c35fba5838be5b4c2fa4bf
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-07-25 10:34:04 +0100

    chore: reconciled spaceship setup to oh-my-zsh instructions only

--- common/setupSpaceshipPrompt.sh
+++ common/setupSpaceshipPrompt.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # https://github.com/spaceship-prompt/spaceship-prompt
+# https://github.com/spaceship-prompt/spaceship-prompt/issues/351#issuecomment-360202618
 
-if [ ! -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" ]; then
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
+  ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
 fi
 
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
 printf "\n"
