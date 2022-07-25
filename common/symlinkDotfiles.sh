5ca268dadeba31302f6b8b8fd501bc0d68e923ce
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-07-25 05:43:17 +0100

    chore: zsh reloaded after dotfiles

--- common/symlinkDotfiles.sh
+++ common/symlinkDotfiles.sh
@@ -2,12 +2,18 @@
 
 # Requires node and dotfiles repo from my github
 
-if [ -d "$HOME/Desktop/olaolu_dev/dotfiles" ] && command -v node &>/dev/null; then
+if [ -d "$HOME/Desktop/olaolu_dev/dotfiles" ] && [ -v $DEV ] && command -v node &>/dev/null; then
   echo "Creating symlinks for dotfiles"
   node "$HOME/Desktop/olaolu_dev/dotfiles/makeSymlinks.mjs"
   echo "Symlinks created"
+
+  [ -d "$HOME/.oh-my-zsh" ] && {
+    echo "Reloading zsh environment..."
+    exec zsh
+  }
 else
   echo "Looks like you are yet to pull down your dotfiles repo. Or maybe Node isn't installed?"
   exit 1
 fi
+
 printf "\n"
