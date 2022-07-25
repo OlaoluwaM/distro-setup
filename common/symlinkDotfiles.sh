2b4155ac581d4b22cd805d56aea2f7a91303800f
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-07-25 04:17:34 +0100

    chore: fixed dotfiles symlink script

--- common/symlinkDotfiles.sh
+++ common/symlinkDotfiles.sh
@@ -4,7 +4,7 @@
 
 if [ -d "$HOME/Desktop/olaolu_dev/dotfiles" ] && command -v node &>/dev/null; then
   echo "Creating symlinks for dotfiles"
-  node "$HOME/Desktop/olaolu_dev/dotfiles/makeSymlinks.js"
+  node "$HOME/Desktop/olaolu_dev/dotfiles/makeSymlinks.mjs"
   echo "Symlinks created"
 else
   echo "Looks like you are yet to pull down your dotfiles repo. Or maybe Node isn't installed?"
