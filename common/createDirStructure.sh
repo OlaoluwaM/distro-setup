8eb72dd77dc3304f99dc3f7f964b6fbcb9985661
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-07-25 18:03:13 +0100

    chore: update

--- common/createDirStructure.sh
+++ common/createDirStructure.sh
@@ -8,7 +8,7 @@ createFileIfDoesNotExist() {
   [ ! -f "$HOME/$1" ] && touch "$HOME/$1" || echo "file $HOME/$1 already exists"
 }
 
-directories=("customizations" ".themes" "Desktop/olaolu_dev" "Desktop/olaolu_dev/dev" "AppImages" "Downloads/others" "Desktop/olaolu_dev/dev/frontend-challenges" "Desktop/olaolu_dev/dev/frontend-challenges/in-progress")
+directories=("customizations" ".themes" "Desktop/olaolu_dev" "Desktop/olaolu_dev/dev" "AppImages" "Desktop/olaolu_dev/dev/frontend-challenges" "Desktop/olaolu_dev/dev/frontend-challenges/in-progress")
 
 echo "Creating directories"
 
