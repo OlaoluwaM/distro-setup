6c9d8c7f01651185066fcc6e06879b5c01ffe5e9
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-07-25 04:38:31 +0100

    chore: fixed global npm install script

--- common/installGlobalNpmPackages.sh
+++ common/installGlobalNpmPackages.sh
@@ -6,10 +6,9 @@ if command -v node &>/dev/null && command -v npm &>/dev/null; then
   echo "Installing global npm packages"
 
   PACKAGES="$DOTFILES/npm/global-npm-pkgs.txt"
-  PACKAGE_PREFIX=$(npm root -g)
 
   while read -r package; do
-    packageName=${package#"$PACKAGE_PREFIX"}
+    packageName=${package#*'node_modules/'}
 
     if (npm list -g --depth=0 | grep "$packageName") &>/dev/null; then
       echo "Seems like $packageName is already installed. Skipping...."
