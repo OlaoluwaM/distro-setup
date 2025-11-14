619dc398f37b9a28d203e9144e8711903b9c18d7
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2025-11-14 05:47:45 -0600

    fix: Enhance installation script for Nvidia container toolkit by adding checks and improving error handling

--- src/distros/fedora/scripts/installNvidiaContainerKit.sh
+++ src/distros/fedora/scripts/installNvidiaContainerKit.sh
@@ -10,8 +10,8 @@ if ! isProgramInstalled curl || ! isProgramInstalled docker; then
 	exit 1
 fi
 
-if isPackageInstalled nvidia-container-toolkit; then
-	echo "Nvidia container toolkit has already been installed. Skipping..."
+if isPackageInstalled nvidia-container-toolkit && isProgramInstalled nvidia-ctk && isProgramInstalled nvidia-smi; then
+	echo "Nvidia container toolkit has already been installed and setup. Skipping..."
 	return
 fi
 
@@ -24,6 +24,7 @@ echo "Installing nvidia-container-toolkit package..."
 # sudo dnf install -y nvidia-container-toolkit
 NVIDIA_CONTAINER_TOOLKIT_VERSION=1.18.0-1
 sudo dnf install -y \
+	nvidia-container-toolkit \
 	nvidia-container-toolkit-${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
 	nvidia-container-toolkit-base-${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
 	libnvidia-container-tools-${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
@@ -31,8 +32,14 @@ sudo dnf install -y \
 echo -e "Done!\n"
 
 echo "Configuring docker to use the nvidia-container-toolkit..."
-sudo nvidia-ctk runtime configure --runtime=docker
-echo -e "Done!\n"
+if sudo nvidia-ctk runtime configure --runtime=docker; then
+	echo -e "Done!\n"
+else
+	echo "There was an error configuring the nvidia-container-toolkit runtime for docker. Please check the output above for more information. Perhaps we don't yet have nvidia drivers setup yet?"
+	# Per the prerequisites section of the install guide https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#prerequisites
+	echo "Exiting..."
+	exit 1
+fi
 
 echo "Restarting docker service..."
 sudo systemctl restart docker
