05f61934e50923d193224953642efd4d72e8e68c
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2026-05-12 22:56:00 -0500

    refactor: Simplify AstroNvim installation process by removing backup logic

--- src/steps/astroNvimSetup.sh
+++ src/steps/astroNvimSetup.sh
@@ -26,31 +26,8 @@ else
 fi
 echo -e "\n"
 
-if git ls-remote git@github.com:OlaoluwaM/nvim-setup.git &>/dev/null; then
-	echo "Installing AstroNvim from our git repo (https://github.com/OlaoluwaM/nvim-setup)..."
-	runOrFail "Could not clone nvim-setup into $nvimConfigDir." git clone git@github.com:OlaoluwaM/nvim-setup.git "$nvimConfigDir"
-	echo -e "Updating AstroNvim dependencies...\n"
-	runOrFail "Could not update AstroNvim dependencies." nvim +AstroUpdate
-	success "AstroNvim dependencies updated"
-	return
-fi
-
-echo "Installing AstroNvim..."
-for nvimPath in "$HOME/.config/nvim" "$HOME/.local/share/nvim" "$HOME/.local/state/nvim" "$HOME/.cache/nvim"; do
-	if ! test -e "$nvimPath"; then
-		continue
-	fi
-
-	backupPath="$nvimPath.bak"
-	if test -e "$backupPath"; then
-		backupPath="$nvimPath.bak.$(date +%Y%m%d%H%M%S)"
-	fi
-
-	runOrFail "Could not move $nvimPath to $backupPath." mv "$nvimPath" "$backupPath"
-done
-
-runOrFail "Could not clone the AstroNvim template into $nvimConfigDir." git clone --depth 1 https://github.com/AstroNvim/template "$nvimConfigDir"
-removePath "$HOME/.config/nvim/.git"
+echo "Installing AstroNvim from our git repo (https://github.com/OlaoluwaM/nvim-setup)..."
+runOrFail "Could not clone nvim-setup into $nvimConfigDir." git clone git@github.com:OlaoluwaM/nvim-setup.git "$nvimConfigDir"
 
 echo "Updating AstroNvim dependencies..."
 runOrFail "Could not update AstroNvim dependencies." nvim +AstroUpdate
